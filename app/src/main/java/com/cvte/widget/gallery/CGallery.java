1d5fd3a32addbf96ef4b95ca31dce2ff75970c87
kuangzukai <kuangzukai@cvte.com>
2017-08-07 15:48:44 +0800

    忘记提交

---
+++ app/src/main/java/com/cvte/widget/gallery/CGallery.java
@@ -0,0 +1,19 @@
+package com.cvte.widget.gallery;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by jokinkuang on 2017/8/7.
+ */
+
+public class CGallery extends FancyCoverFlow {
+    public CGallery(Context context) {
+        super(context);
+    }
+
+    public CGallery(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
